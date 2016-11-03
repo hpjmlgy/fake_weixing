cb6f564a3eec9b78c2e90d719cb9babcd6687e77
hpjmlgy <hepeijun2@hotmail.com>
2016-11-03 16:47:04 +0800

    add优惠券

--- src/module/yhj/Yhj.js
+++ src/module/yhj/Yhj.js
@@ -5,7 +5,25 @@ injectTapEventPlugin();
 
 
 var Yhj = React.createClass({
+  getInitialState: function() {
+    return {
+      null
+    }
+  },
+  handleTap: function(e) {
+    var target = $(e.target).parent().next();
+
+    switch (target.css("display")) {
+      case "none":
+        target.css("display", "block")
+        break;
+      case "block":
+        target.css("display", "none")
+        break;
+    }
+  },
   render: function() {
+    var o = this;
     var coupon_num = this.props.coupon.length;
     if (coupon_num == 0) {
 
@@ -20,22 +38,32 @@ var Yhj = React.createClass({
     } else {
 
       var coupons = this.props.coupon.map(function(elem) {
+        var item_style = {
+          display: 'none'
+        };
+
         return (
           <section className="coupon_wrapper">
                  <div className="ticket_top_module">
                     <div className="left_wrapper">
-                       <span>elem.name</span>
+                       <span>{elem.name}</span>
                        <span>代金券</span>
-                       <span>elem.condition</span>
+                       <span>{elem.condition}</span>
                     </div>
                     <div className="right_wrapper">
-                       <span>elem.price</span>
+
+                       <span>{elem.price}</span>
                        <span>￥</span>
+                       
                     </div>
                  </div>
                  <div className="ticket_bot_module">
                     <span>有效期至{elem.date}</span>
-                    <span>使用规则</span>
+                    <span className="arrow" onTouchTap={o.handleTap}></span>
+                    <span  >使用规则</span>
+                 </div>
+                 <div className="condition_desc" style={item_style}>
+                    <span>使用规则使用规则使用规则使用规则使用规则使用规则使用规则</span>
                  </div>
           </section>
         );
--- src/yhj.js
+++ src/yhj.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import Yhj from './module/yhj/Yhj'
 
 ReactDOM.render(
-	<Yhj coupon={[]}/>,
+	<Yhj coupon={[{name:"炫彩冲印",condition:"满100可使用",price:30,date:"2016.10.03"},{name:"炫彩冲印",condition:"满100可使用",price:30,date:"2016.10.03"}]}/>,
 	document.getElementById('yhj_wrapper')
 );
